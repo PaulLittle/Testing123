87205c911f52219129a4426f4642a7aa74e9e5c7
PaulLittle <S00156762@mail.itsligo.ie>
2016-10-21 17:20:08 +0100

    Add project files.

---
+++ src/SAC_Web_Application/Models/ManageViewModels/VerifyPhoneNumberViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAC_Web_Application.Models.ManageViewModels
+{
+    public class VerifyPhoneNumberViewModel
+    {
+        [Required]
+        public string Code { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; }
+    }
+}
