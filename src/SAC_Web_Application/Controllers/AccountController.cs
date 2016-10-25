d41b4394480462a17ac9a860564dc8ab41a33214
PaulLittle <S00156762@mail.itsligo.ie>
2016-10-25 21:42:37 +0100

    Updated Registration to Show Email Sent Message

--- src/SAC_Web_Application/Controllers/AccountController.cs
+++ src/SAC_Web_Application/Controllers/AccountController.cs
@@ -128,7 +128,9 @@ namespace SAC_Web_Application.Controllers
                         $"Please confirm your account by clicking this link: <a href='{callbackUrl}'>link</a>");
                     //await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation(3, "User created a new account with password.");
-                    return RedirectToLocal(returnUrl);
+                    //return RedirectToLocal(returnUrl);
+                    ModelState.AddModelError(string.Empty, "An email has been sent to your account, please follow the link to confirm registration.");
+                    return View(model);
                 }
                 AddErrors(result);
             }
